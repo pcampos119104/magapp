effb11937f378c517e5c29f6b5e3bf11fe677a65
pcampos <pcampos119104@gmail.com>
2022-04-23 09:21:13 -0300

    minor refactor

--- magapp/ingredients/models/Ingredient.py
+++ magapp/ingredients/models/Ingredient.py
@@ -1,14 +1,20 @@
 from django.db import models
 
 from magapp.core.models import ModelBase
+from magapp.core.utils import create_unique_slug
 
 
 class Ingredient(ModelBase):
     name = models.CharField("nome", max_length=64, help_text="Ingrediente e quantidade")
+    slug = models.SlugField(max_length=64, unique=True)
 
     class Meta:
         verbose_name = "ingrediente"
         verbose_name_plural = "ingredientes"
+
+    def save(self, *args, **kwargs):
+        self.slug = create_unique_slug(self.title)
+        super(Ingredient, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
--- magapp/recipes/models/Recipe.py
+++ magapp/recipes/models/Recipe.py
@@ -1,16 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.forms import model_to_dict
-from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 from magapp.core.models import ModelBase
+from magapp.core.utils import create_unique_slug
 
 
 class Recipe(ModelBase):
     title = models.CharField("título", max_length=64, help_text="Título da receita.")
     directions = models.TextField("preparo", help_text="Passos para o preparo.")
-    slug = models.SlugField(max_length=64, unique=True)
+    slug = models.SlugField(max_length=64, unique=True, editable=False)
     ingredients = models.ManyToManyField(
         "ingredients.Ingredient", through="recipes.RecipeIngredient"
     )
@@ -29,9 +29,8 @@ class Recipe(ModelBase):
         verbose_name = "receita"
         verbose_name_plural = "receitas"
 
-    # todo make slug unique even with same title
     def save(self, *args, **kwargs):
-        self.slug = get_unique_slug(self.pk, self.title, Recipe.all_objects)
+        self.slug = create_unique_slug(self.title)
         super(Recipe, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -42,15 +41,3 @@ class Recipe(ModelBase):
 
     def get_absolute_url(self):
         return reverse("recipes:detail", kwargs={"slug": self.slug})
-
-
-def get_unique_slug(pk, title, manager):
-    slug = slugify(title)
-    unique_slug = slug
-    counter = 2
-    while manager.filter(slug=unique_slug).exists():
-        if manager.filter(slug=unique_slug).values("pk")[0]["pk"] == pk:
-            break
-        unique_slug = "{}-{}".format(slug, counter)
-        counter += 1
-    return unique_slug
