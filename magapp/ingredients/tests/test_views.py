09b29df361d1f405b007e0412dd228118e5dd692
pcampos <pcampos119104@gmail.com>
2022-06-19 09:13:27 -0300

    finish ingredient

--- magapp/ingredients/tests/test_views.py
+++ magapp/ingredients/tests/test_views.py
@@ -5,42 +5,44 @@ from django.urls import reverse
 from model_bakery import baker
 
 from magapp.django_assertions import assert_contains
-from magapp.recipes.models import Recipe
+from magapp.ingredients.models import Ingredient
 
 pytestmark = pytest.mark.django_db
 
 
-class TestRecipesViews:
+class TestIngredientsViews:
     def test_list_view(self, client):
-        recipes = baker.make(
-            Recipe, 5, title=str(uuid.uuid4())[:6], draft=False, deleted_at=None
+        ingredients = baker.make(
+            Ingredient, 5, name=str(uuid.uuid4())[:6], deleted_at=None
         )
-        resp = client.get(reverse("recipes:list"))
+        resp = client.get(reverse("ingredients:list"))
         assert resp.status_code == 200
-        assert_contains(resp, recipes[0].title)
-        assert_contains(resp, recipes[0].get_absolute_url())
-        assert_contains(resp, recipes[-1].title)
-        assert_contains(resp, recipes[-1].get_absolute_url())
+        assert_contains(resp, ingredients[0].name)
+        assert_contains(resp, ingredients[0].get_absolute_url())
+        assert_contains(resp, ingredients[-1].name)
+        assert_contains(resp, ingredients[-1].get_absolute_url())
 
     @pytest.mark.skip
-    def test_detail_view(self, client, recipe: Recipe):
-        resp = client.get(reverse("recipes:detail", args=(recipe.slug,)))
+    def test_detail_view(self, client, ingredient: Ingredient):
+        resp = client.get(reverse("ingredients:detail", args=(ingredient.slug,)))
         assert resp.status_code == 200
-        assert_contains(resp, recipe.title)
+        assert_contains(resp, ingredient.title)
 
     def test_create_view_no_login(self, client):
-        resp = client.get(reverse("recipes:create"))
+        resp = client.get(reverse("ingredients:create"))
         assert resp.status_code == 302
 
     def test_create_view_with_login(self, logged_client):
-        resp = logged_client.get(reverse("recipes:create"))
+        resp = logged_client.get(reverse("ingredients:create"))
         assert resp.status_code == 200
-        assert_contains(resp, "id_title")
+        assert_contains(resp, "id_name")
 
     @pytest.mark.skip
     def test_delete_view(self, logged_client):
-        recipe = baker.make(Recipe, _fill_optional=True, is_removed=False)
-        resp = logged_client.delete(reverse("recipes:delete", args=(recipe.id,)))
+        ingredient = baker.make(Ingredient, _fill_optional=True, is_removed=False)
+        resp = logged_client.delete(
+            reverse("ingredients:delete", args=(ingredient.id,))
+        )
         assert_contains(resp, '"ok": true')
 
 
