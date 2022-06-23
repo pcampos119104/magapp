55eb6a67a0bc869dff31e92169c3252a7e14ac99
pcampos <pcampos119104@gmail.com>
2022-06-23 06:44:59 -0300

    added recipe to template

--- magapp/recipes/filters.py
+++ magapp/recipes/filters.py
@@ -4,6 +4,5 @@ from django import forms
 
 class RecipeFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(
-        widget=forms.TextInput(attrs={"class": "form-control bg-light border-0 small"}),
         lookup_expr="icontains",
     )
