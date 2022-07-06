c0e95474351041633b12bdce98c4f7a594c68b12
pcampos <pcampos119104@gmail.com>
2022-07-06 06:51:04 -0300

    update all relevant templates for mvp

--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="base.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
     # grappelli URLS
     path("grappelli/", include("grappelli.urls")),
     # Django Admin, use {% url 'admin:index' %}
