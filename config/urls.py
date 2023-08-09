465a21c1ab6720a0bee1862f21eaf71fa92b653c
pcampos <pcampos119104@gmail.com>
2023-08-09 08:22:18 -0300

    redirect home to recipes

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="base.html"), name="home"),
+    path("", RedirectView.as_view(url="/recipes/"), name="home"),
     # grappelli URLS
     path("grappelli/", include("grappelli.urls")),
     # Django Admin, use {% url 'admin:index' %}
