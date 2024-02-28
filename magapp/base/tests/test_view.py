265b5b4e1e6377de7c6e067ade73745be2d1592d
pcampos <pcampos119104@gmail.com>
2024-02-28 08:24:01 -0300

    Adicionado primeiro teste

---
+++ magapp/base/tests/test_view.py
@@ -0,0 +1,13 @@
+
+import uuid
+
+import pytest
+from django.urls import reverse
+
+pytestmark = pytest.mark.views
+
+
+class TestBaseViews:
+    def test_home(self, client):
+        resp = client.get(reverse("base:home"))
+        assert resp.status_code == 200
