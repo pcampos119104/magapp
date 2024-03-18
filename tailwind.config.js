b664f1e6cf221772014aa5b4b12894d64132c9f6
pcampos <pcampos119104@gmail.com>
2024-03-18 08:59:21 -0300

    adicionado tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./magapp/templates/**/*.{html,js}",
+      "./magapp/templates/**/*.{html,js}",
+      ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
