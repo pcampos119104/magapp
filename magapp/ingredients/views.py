87552db4abe206657f8f1e139b86c93887100a3e
pcampos <pcampos119104@gmail.com>
2024-03-21 11:04:21 -0300

    criado login layout

--- magapp/ingredients/views.py
+++ magapp/ingredients/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import View
 
 
+@login_required
 def list(request):
     template = 'ingredients/list.html'
     return render(request, template)
