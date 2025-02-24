595e5521d1637fe44dc92d95ad5620acc9ee7806
pcampos <pcampos119104@gmail.com>
2025-02-24 09:51:10 -0300

    minor fix

--- magapp/settings.py
+++ magapp/settings.py
@@ -154,9 +154,6 @@ ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_CHANGE_EMAIL = True
 ACCOUNT_EMAIL_VERIFICATION = None
 # https://forum.djangoproject.com/t/cannot-access-admin-if-setting-is-set-cannot-access-website-if-setting-is-not-set/24496
-SECURE_SSL_REDIRECT = not DEBUG
-SESSION_COOKIE_SECURE = not DEBUG
-CSRF_COOKIE_SECURE = not DEBUG
 
 LOGIN_REDIRECT_URL = '/'
 
